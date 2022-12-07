985495b3ef0eb10bf65344b8077ded19c2204716
Afganli97 <ulviva@code.edu.az>
2022-12-08 01:28:29 +0400

    complete location box visibility

--- Home/script.js
+++ Home/script.js
@@ -1,4 +1,11 @@
-let location = document.querySelector(".location");
-location.onclick=function(){
-    
+let locationClose = document.querySelector(".x-custom");
+let loc = document.querySelector(".location");
+let locationBackground = document.querySelector(".location-background");
+let locationBox = document.querySelector(".location-box");
+loc.onclick = function () {
+    locationBackground.classList.remove("close");
 };
+locationClose.onclick = function(){
+    locationBackground.classList.add("close");
+};
+
