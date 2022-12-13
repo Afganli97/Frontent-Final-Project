a748ae8ee7b55ca37e03d74f15adbe1416e58fb3
Afganli97 <ulviva@code.edu.az>
2022-12-13 16:30:12 +0400

    complete account page

--- account/script.js
+++ account/script.js
@@ -4,6 +4,9 @@ let locationBackground = document.querySelector(".location-background");
 let locationBox = document.querySelector(".location-box");
 let cat = document.querySelector(".categories");
 let catDrop = document.querySelector(".categories-drop");
+let login = document.querySelector(".login-text");
+let register = document.querySelector(".login-register");
+let loginBox = document.querySelector(".login-animate");
 loc.onclick = function () {
     locationBackground.classList.remove("close");
 };
@@ -12,4 +15,14 @@ locationClose.onclick = function(){
 };
 cat.onclick = function(){
         catDrop.classList.toggle("close");
+};
+register.onclick = function(){
+    loginBox.style.left = "-100%";
+    register.style.color = "black";
+    login.style.color = "gray";
+}
+login.onclick = function(){
+    loginBox.style.left = "0";
+    register.style.color = "gray";
+    login.style.color = "black";
 }
